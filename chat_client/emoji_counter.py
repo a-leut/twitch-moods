9a0112da5484d3a17fd9b309744839782f062043
a-leut <turbomonkeyninja@gmail.com>
2016-04-28 11:17:04 -0600

    expire kind doing something

--- chat_client/emoji_counter.py
+++ chat_client/emoji_counter.py
@@ -13,18 +13,22 @@ class EmojiCounter(object):
 
     def update_emoji_count(self, message):
         tokens = message.split()
-        emoji_counts = Counter([t for t in tokens if t in self.emojis])
-        ts = time.time()
-        for emoji, count in emoji_counts.iteritems():
-            self._add_key(ts, emoji, count)
-        # update redis from counts
-        for emoji in self.emojis:
-            self._delete_old_keys(emoji, ts)
-            self._update_count(emoji, ts)
+        emojis = [t for t in tokens if t in self.emojis]
+        if emojis:
+            emoji_counts = Counter(emojis)
+            ts = time.time()
+            for emoji, count in emoji_counts.most_common():
+                self._add_key(ts, emoji, count)
+            # update redis from counts
+            for emoji in self.emojis:
+                self._delete_old_keys(emoji, ts)
+                self._update_count(emoji, ts)
 
     def _update_count(self, emoji, ts):
         count = self._redis.zcount('s_' + emoji, 0, ts)
         self._redis.set(emoji, count)
+        if count > 0:
+            print(emoji, count)
 
     def _delete_old_keys(self, emoji, ts, expire=60):
         self._redis.zremrangebyscore('s_' + emoji, 0, ts - expire)
@@ -37,9 +41,10 @@ class EmojiCounter(object):
         """ Adds 'count' keys to the sorted set corresponding with 'emoji.'
             Unique names are given to the keys when 'count' > 1
         """
+        print(time, emoji, count)
         if count == 1:
-            self._redis.zadd('s_' + emoji, 'emoji', time)
+            self._redis.zadd('s_' + emoji, time, time)
         elif count > 1:
             # TODO: refactor to single call
-            for n in count:
-                self._redis.zadd('s_' + emoji, 'emoji' + n, time)
+            for n in range(count):
+                self._redis.zadd('s_' + emoji, time - 0.001 * n, time)
--- web_app/views.py
+++ web_app/views.py
@@ -6,16 +6,16 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 names, urls = get_emoji_names_urls()
 
 def get_results(cutoff=40):
-    """ Returns json form emoji counts from redis for count in the top `cutoff`
-        percentile and above
+    """ Returns json of emoji counts from redis for counts in the top
+        'cutoff' percentile of total counts and above
     """
     counts = current_app.redis.mget(names)
     n_counts = np.array([int(c) for c in counts])
-    n_counts = n_counts[np.nonzero(n_counts)]  # remove 0s
+    n_counts = n_counts[np.nonzero(n_counts)]
     percentile = np.percentile(n_counts, cutoff)
     results = {}
     for i, count in enumerate(counts):
-        if count and int(count) > percentile:
+        if count and int(count) >= percentile:
             results[urls[i]] = int(count)
     return jsonify(results)
 
