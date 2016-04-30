b8a83c32b6330d673f0d95cdc32db424ff44d77d
a-leut <turbomonkeyninja@gmail.com>
2016-04-30 16:44:34 -0600

    refactored to only update known emojis

--- chat_client/emoji_counter.py
+++ chat_client/emoji_counter.py
@@ -5,19 +5,27 @@ from twitch_api_service import get_emoji_names_urls
 
 class EmojiCounter(object):
     """ Counts emojis and stores the result in redis through the lifetime of
-    the object.
+        the object.
+
+        Timestamps of emojis are stored in sorted sets named "l_[emoji]" and
+        counts of these timestamps are stored in keys named "[emoji]."
     """
-    BUFFER = 40
+    BUFFER_LIMIT = 40
 
     def __init__(self, redis_connection):
         self.emojis, _ = get_emoji_names_urls()
         self._redis = redis_connection
         self._reset_redis()
         self._messages = []
+        # Set of emojis for which there are non-empty sorted sets of timestamps
+        self._known_emojis = set()
 
     def update_emoji_count(self, message):
+        """ Adds message to message buffer. Processes and resets the buffer
+            once we reach a certain number of messages.
+        """
         self._messages.append(message)
-        if len(self._messages) > EmojiCounter.BUFFER:
+        if len(self._messages) > EmojiCounter.BUFFER_LIMIT:
             print('Updating redis')
             self.process_messages()
             self._messages = []
@@ -31,26 +39,30 @@ class EmojiCounter(object):
             emoji_counts = Counter(emojis)
             ts = time.time()
             for emoji, count in emoji_counts.most_common():
-                self._add_key(ts, emoji, count)
-            # Update redis from counts
-            for emoji in self.emojis:
+                self._known_emojis.add(emoji)
+                self._add_timestamp(ts, emoji, count)
+            # Update count keys from sorted sets
+            for emoji in self._known_emojis:
                 self._delete_old_keys(emoji, ts)
-                self._update_api_count(emoji, ts)
+                count = self._update_api_count(emoji, ts)
+                if count == 0:
+                    self._known_emojis.remove(emoji)
 
     def _update_api_count(self, emoji, ts):
         count = self._redis.zcount('s_' + emoji, 0, ts)
         self._redis.set(emoji, count)
-        if count > 0:
-            print('Set %s to %s' % (emoji, count))
+        print('Set %s to %s' % (emoji, count))
+        return count
 
     def _delete_old_keys(self, emoji, ts, expire=60):
         self._redis.zremrangebyscore('s_' + emoji, 0, ts - expire)
 
     def _reset_redis(self):
         for emoji in self.emojis:
+            self._redis.zremrangebyrank('s_' + emoji, -1, 0)
             self._redis.set(emoji, 0)
 
-    def _add_key(self, time, emoji, count):
+    def _add_timestamp(self, time, emoji, count):
         """ Adds 'count' keys to the sorted set corresponding with 'emoji.'
             Unique names are given to the keys when 'count' > 1
         """
@@ -60,4 +72,5 @@ class EmojiCounter(object):
         elif count > 1:
             # TODO: refactor to single call
             for n in range(count):
-                self._redis.zadd('s_' + emoji, time - 0.001 * n, time)
+                unique_time = time - 0.001 * n
+                self._redis.zadd('s_' + emoji, unique_time, unique_time)
